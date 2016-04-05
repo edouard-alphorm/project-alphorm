aa9eafaa913ffc4a7d2c72767db5a921a3d63f61
Edouard FERRARI <edouard@ferrari.wf>
2016-04-05 10:54:37 -0700

    Changement du message d'erreur en cas de 404

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use('/groups', groups);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
+  var err = new Error('That is not Found');
   err.status = 404;
   next(err);
 });
