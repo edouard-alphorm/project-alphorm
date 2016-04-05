43cd871c4f0bf447376bd9757db0095b4c5d997f
Edouard FERRARI <edouard@ferrari.wf>
2016-04-05 12:51:51 -0700

    Suppression du commentaire

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- var express = require('express');
+var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -14,8 +14,6 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'coffee');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
