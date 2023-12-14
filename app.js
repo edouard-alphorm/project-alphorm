966cb2996dc224e2a4b954f4fa47e55d6bd37bdd
izowenger <ismaila14cisse@gmail.com>
2023-12-14 16:48:18 +0100

    lait

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'coffee');
+app.set('view engine', 'lait');
 
 app.use(logger('dev'));
 app.use(bodyParser.json());
